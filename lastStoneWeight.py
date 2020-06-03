ea9bf56f9be2367717bd6c40e1138087a010b6f8
bru <brahada25@gmail.com>
2020-06-03 23:51:35 +0530

    Create lastStoneWeight.py

---
+++ lastStoneWeight.py
@@ -0,0 +1,7 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones.sort()
+        for _ in range(len(stones) - 1):
+            bisect.insort(stones, stones.pop() - stones.pop())
+        return stones[0]
+        
